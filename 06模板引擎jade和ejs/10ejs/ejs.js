7ff3f7774fcb8ed91b475e6c6e0de4c07345c19e
glen <512043920@qq.com>
2017-08-20 13:28:09 +0800

    ejs include

---
+++ 06模板引擎jade和ejs/10ejs/ejs.js
@@ -0,0 +1,23 @@
+/**
+ * Created by lenovo on 2017/8/20.
+ */
+
+const ejs = require('ejs');
+const fs = require('fs');
+
+ ejs.renderFile('./view/demo.ejs', {type: 'admin'}, (err, data) => {
+    if (err)
+        console.log('读取失败');
+
+     writeFile (data);
+});
+
+function writeFile (data) {
+    fs.writeFile('./build/demo.html', data, (err) => {
+        if (err)
+            console.log('写入失败');
+        console.log('写入成功');
+    });
+};
+
+
